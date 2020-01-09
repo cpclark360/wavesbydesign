cd887d35448aa484e8a73a8f5ea811703f3c5b79
Cedric Clark <40728556+cpclark360@users.noreply.github.com>
2020-01-09 08:59:56 -0600

    Add files via upload
    added a javascript function that closes the navbar menu after the user makes a selection.

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,10 +1,9 @@
-(function ($) {
+(function($) {
   "use strict";
 
   var fn = {
-
     // Launch Functions
-    Launch: function () {
+    Launch: function() {
       fn.Header();
       fn.Masonry();
       fn.Overlay();
@@ -15,36 +14,32 @@
       fn.Apps();
     },
 
-
-    Header: function (){
+    Header: function() {
       $(document.body).headroom({
-        tolerance : 10
+        tolerance: 10
       });
     },
-
 
     Masonry: function() {
-      var $grid = $('.masonry').masonry({
-        itemSelector: '.masonry > *',
+      var $grid = $(".masonry").masonry({
+        itemSelector: ".masonry > *"
       });
     },
-
 
     // Owl Carousel
     OwlCarousel: function() {
-
-      $('.owl-carousel').each(function() {
+      $(".owl-carousel").each(function() {
         var a = $(this),
-          items = a.data('items') || [1,1,1,1],
-          margin = a.data('margin'),
-          loop = a.data('loop'),
-          nav = a.data('nav'),
-          dots = a.data('dots'),
-          center = a.data('center'),
-          autoplay = a.data('autoplay'),
-          autoplaySpeed = a.data('autoplay-speed'),
-          rtl = a.data('rtl'),
-          autoheight = a.data('autoheight');
+          items = a.data("items") || [1, 1, 1, 1],
+          margin = a.data("margin"),
+          loop = a.data("loop"),
+          nav = a.data("nav"),
+          dots = a.data("dots"),
+          center = a.data("center"),
+          autoplay = a.data("autoplay"),
+          autoplaySpeed = a.data("autoplay-speed"),
+          rtl = a.data("rtl"),
+          autoheight = a.data("autoheight");
 
         var options = {
           nav: nav || false,
@@ -62,90 +57,98 @@
             0: { items: items[3] || 1 },
             992: { items: items[2] || 1 },
             1200: { items: items[1] || 1 },
-            1600: { items: items[0] || 1}
+            1600: { items: items[0] || 1 }
           }
         };
 
         a.owlCarousel(options);
 
         // Custom Navigation Events
-        $(document).on('click', '.owl-item>div', function() {
-          $owl.trigger('to.owl.carousel', $(this).data( 'position' ) );
+        $(document).on("click", ".owl-item>div", function() {
+          $owl.trigger("to.owl.carousel", $(this).data("position"));
         });
       });
     },
 
     // Overlay Menu
     Overlay: function() {
-      $(document).ready(function(){
-        $('.overlay-menu-open').click(function(){
-          $(this).toggleClass('active');
-          $('html').toggleClass('active');
-          $('.overlay-menu').toggleClass('active');
+      $(document).ready(function() {
+        $(".overlay-menu-open").click(function() {
+          $(this).toggleClass("active");
+          $("html").toggleClass("active");
+          $(".overlay-menu").toggleClass("active");
         });
       });
     },
 
-
     // File Tree
     Filetree: function() {
-      var folder = $('.file-tree li.file-tree-folder'),
-          file = $('.file-tree li');
+      var folder = $(".file-tree li.file-tree-folder"),
+        file = $(".file-tree li");
 
       folder.on("click", function(a) {
-          $(this).children('ul').slideToggle(400, function() {
-              $(this).parent("li").toggleClass("open")
-          }), a.stopPropagation()
-      })
+        $(this)
+          .children("ul")
+          .slideToggle(400, function() {
+            $(this)
+              .parent("li")
+              .toggleClass("open");
+          }),
+          a.stopPropagation();
+      });
 
-      file.on('click', function(b){
+      file.on("click", function(b) {
         b.stopPropagation();
-      })
+      });
     },
-
 
     // Clipboard
     Clipboard: function() {
-      var a = new ClipboardJS('.anchor', {
+      var a = new ClipboardJS(".anchor", {
         text: function(b) {
-          return window.location.host + window.location.pathname + $(b).attr("href")
+          return (
+            window.location.host + window.location.pathname + $(b).attr("href")
+          );
         }
       });
 
-      a.on('success', function(e) {
-        e.clearSelection(), $(e.trigger).addClass("copied"), setTimeout(function() {
-          $(e.trigger).removeClass("copied")
-        }, 2000)
+      a.on("success", function(e) {
+        e.clearSelection(),
+          $(e.trigger).addClass("copied"),
+          setTimeout(function() {
+            $(e.trigger).removeClass("copied");
+          }, 2000);
       });
     },
 
     ImageView: function() {
-      $('.lightbox').magnificPopup({
-        type: 'image',
+      $(".lightbox").magnificPopup({
+        type: "image",
         closeOnContentClick: true,
         closeBtnInside: false,
         fixedContentPos: true,
-        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        mainClass: "mfp-no-margins mfp-with-zoom", // class to remove default margin from left and right side
         image: {
           verticalFit: true
         }
       });
 
-      $('.gallery').each(function() { // the containers for all your galleries
-          $(this).magnificPopup({
-              delegate: '.photo > a', // the selector for gallery item
-              type: 'image',
-              mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-              gallery: {
-                enabled:true
-              }
-          });
+      $(".gallery").each(function() {
+        // the containers for all your galleries
+        $(this).magnificPopup({
+          delegate: ".photo > a", // the selector for gallery item
+          type: "image",
+          mainClass: "mfp-no-margins mfp-with-zoom", // class to remove default margin from left and right side
+          gallery: {
+            enabled: true
+          }
+        });
       });
-      
-      $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+
+      $(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
         disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
+        type: "iframe",
+        mainClass: "mfp-fade",
         removalDelay: 160,
         preloader: false,
 
@@ -153,47 +156,42 @@
       });
     },
 
-
     // Apps
-    Apps: function () {
-
+    Apps: function() {
       // accordion
       $(document).ready(function() {
-
-        $('.collapse').on('show.bs.collapse', function () {
-            $(this).parent().addClass('active');
+        $(".collapse").on("show.bs.collapse", function() {
+          $(this)
+            .parent()
+            .addClass("active");
         });
 
-        $('.collapse').on('hide.bs.collapse', function () {
-            $(this).parent().removeClass('active');
+        $(".collapse").on("hide.bs.collapse", function() {
+          $(this)
+            .parent()
+            .removeClass("active");
         });
-
       });
-
 
       // tooltips
-      $('[data-toggle="tooltip"]').tooltip()
-
-
+      $('[data-toggle="tooltip"]').tooltip();
 
       // skrollr
-      skrollr.init({  
-          forceHeight: false,        
-          mobileCheck: function() {
-              //hack - forces mobile version to be off
-              return false;
-          }
+      skrollr.init({
+        forceHeight: false,
+        mobileCheck: function() {
+          //hack - forces mobile version to be off
+          return false;
+        }
       });
-
 
       // Smooth Scroll
-      $(function () {
-        var scroll = new SmoothScroll('[data-scroll]');
+      $(function() {
+        var scroll = new SmoothScroll("[data-scroll]");
       });
 
-
       // Lavalamp
-      $('.lavalamp').lavalamp({
+      $(".lavalamp").lavalamp({
         setOnClick: true,
         enableHover: false,
         margins: false,
@@ -201,65 +199,63 @@
         duration: 200
       });
 
+      $(document).ready(function() {
+        var window_width = jQuery(window).width();
 
-      $(document).ready(function(){
-          var window_width = jQuery( window ).width();
+        if (window_width < 768) {
+          $(".sticky").trigger("sticky_kit:detach");
+        } else {
+          make_sticky();
+        }
+
+        $(window).resize(function() {
+          window_width = jQuery(window).width();
 
           if (window_width < 768) {
             $(".sticky").trigger("sticky_kit:detach");
           } else {
             make_sticky();
           }
+        });
 
-
-          $( window ).resize(function() {
-
-            window_width = jQuery( window ).width();
-
-            if (window_width < 768) {
-              $(".sticky").trigger("sticky_kit:detach");
-            } else {
-              make_sticky();
-            }
-
-          });
-
-
-          // recalc on collapse
-          $('.nav-item .collapse').on('shown.bs.collapse hidden.bs.collapse', function () {
+        // recalc on collapse
+        $(".nav-item .collapse").on(
+          "shown.bs.collapse hidden.bs.collapse",
+          function() {
             $(".sticky").trigger("sticky_kit:recalc");
-          });
-
-          function make_sticky() {
-            $(".sticky").stick_in_parent();
           }
+        );
 
+        function make_sticky() {
+          $(".sticky").stick_in_parent();
+        }
       });
 
-
       // prism
-      (function(){
-        if (typeof self === 'undefined' || !self.Prism || !self.document) {
+      (function() {
+        if (typeof self === "undefined" || !self.Prism || !self.document) {
           return;
         }
 
         if (!Prism.plugins.toolbar) {
-          console.warn('Copy to Clipboard plugin loaded before Toolbar plugin.');
+          console.warn(
+            "Copy to Clipboard plugin loaded before Toolbar plugin."
+          );
 
           return;
         }
 
         var ClipboardJS = window.ClipboardJS || undefined;
 
-        if (!ClipboardJS && typeof require === 'function') {
-          ClipboardJS = require('clipboard');
+        if (!ClipboardJS && typeof require === "function") {
+          ClipboardJS = require("clipboard");
         }
 
         var callbacks = [];
 
         if (!ClipboardJS) {
-          var script = document.createElement('script');
-          var head = document.querySelector('head');
+          var script = document.createElement("script");
+          var head = document.querySelector("head");
 
           script.onload = function() {
             ClipboardJS = window.ClipboardJS;
@@ -271,13 +267,16 @@
             }
           };
 
-          script.src = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js';
+          script.src =
+            "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js";
           head.appendChild(script);
         }
 
-        Prism.plugins.toolbar.registerButton('copy-to-clipboard', function (env) {
-          var linkCopy = document.createElement('a');
-          linkCopy.textContent = 'Copy';
+        Prism.plugins.toolbar.registerButton("copy-to-clipboard", function(
+          env
+        ) {
+          var linkCopy = document.createElement("a");
+          linkCopy.textContent = "Copy";
 
           if (!ClipboardJS) {
             callbacks.push(registerClipboard);
@@ -289,26 +288,26 @@
 
           function registerClipboard() {
             var clip = new ClipboardJS(linkCopy, {
-              'text': function () {
+              text: function() {
                 return env.code;
               }
             });
 
-            clip.on('success', function() {
-              linkCopy.textContent = 'Copied!';
+            clip.on("success", function() {
+              linkCopy.textContent = "Copied!";
 
               resetText();
             });
-            clip.on('error', function () {
-              linkCopy.textContent = 'Press Ctrl+C to copy';
+            clip.on("error", function() {
+              linkCopy.textContent = "Press Ctrl+C to copy";
 
               resetText();
             });
           }
 
           function resetText() {
-            setTimeout(function () {
-              linkCopy.textContent = 'Copy';
+            setTimeout(function() {
+              linkCopy.textContent = "Copy";
             }, 5000);
           }
         });
@@ -316,8 +315,26 @@
     }
   };
 
-  $(document).ready(function () {
+  $(document).ready(function() {
     fn.Launch();
   });
-
 })(jQuery);
+
+// Event Listener for navbar
+// closes Navbar menu after the user makes a selection.
+
+// nav className
+let Nav = document.getElementsByTagName("nav")[0].className;
+
+// parent element to great parent to ul containing navigation menu
+const menu = document.getElementById("navbarSupportedContent");
+
+console.log(menu);
+console.log(Nav);
+
+menu.addEventListener("click", () => {
+  setTimeout(() => {
+    menu.className = "collapse navbar-collapse";
+  }, 100);
+  console.log(menu.className);
+});
